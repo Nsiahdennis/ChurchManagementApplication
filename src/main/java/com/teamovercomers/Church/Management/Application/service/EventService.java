aeb8041edf776a9b6d6e1cf11be5275219a93380
Nsiah Kofi <nsiahkofidennis001@gmail.com>
2024-07-17 17:25:46 +0000

    second commit

--- src/main/java/com/teamovercomers/Church/Management/Application/service/EventService.java
+++ src/main/java/com/teamovercomers/Church/Management/Application/service/EventService.java
@@ -1,6 +1,5 @@
 package com.teamovercomers.Church.Management.Application.service;
 
-import com.teamovercomers.Church.Management.Application.entity.Church;
 import com.teamovercomers.Church.Management.Application.entity.Event;
 import com.teamovercomers.Church.Management.Application.repository.EventRepository;
 import org.springframework.stereotype.Service;
@@ -11,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import java.util.List;
 
 @Service
-public class EventService {
+public class  EventService {
     @Autowired
     private EventRepository eventRepository;
     public List<Event>getAllEvent(){
@@ -20,9 +19,10 @@ public class EventService {
     public Event getById(Integer id){
         return eventRepository.findById(Long.valueOf(id)).orElse(null);
     }
-    //@PostMapping
-   // public Event createEvent(@RequestBody Event event) {
+    @PostMapping
+   public Event createEvent(@RequestBody Event event) {
 
-       // return eventService.createEvent(event);
+        EventService eventService = new EventService();
+        return eventService.createEvent(event);
     }
-//}
+}
