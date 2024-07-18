c5a8f36f64a6521cbbf4f6a890188b9480e24e59
Nsiah Kofi <nsiahkofidennis001@gmail.com>
2024-07-18 17:46:42 +0000

    first commit

--- src/main/java/com/teamovercomers/Church/Management/Application/service/MinistryDepartmentService.java
+++ src/main/java/com/teamovercomers/Church/Management/Application/service/MinistryDepartmentService.java
@@ -1,4 +1,43 @@
 package com.teamovercomers.Church.Management.Application.service;
 
+import com.teamovercomers.Church.Management.Application.entity.Church;
+import com.teamovercomers.Church.Management.Application.entity.MinistryDepartment;
+import com.teamovercomers.Church.Management.Application.repository.MinistryDepartmentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class MinistryDepartmentService {
+
+    @Autowired
+    private MinistryDepartmentRepository ministryDepartmentRepository;
+
+    private List<MinistryDepartment> getAllMinistryDepartment() {
+        return ministryDepartmentRepository.findAll();
+    }
+
+    public MinistryDepartment getMinistryDepartmentById(Long id) {
+        return ministryDepartmentRepository.findById(id).orElse(null);
+    }
+
+    public MinistryDepartment createMinistryDepartment(MinistryDepartment ministryDepartment) {
+        return ministryDepartmentRepository.save(ministryDepartment);
+    }
+
+    public MinistryDepartment updateMinistryDepartment(Long id, MinistryDepartment ministryDepartment) {
+        MinistryDepartment existingMinistryDepartment = ministryDepartmentRepository.findById(id).orElse(null);
+        if (existingMinistryDepartment != null) {
+            existingMinistryDepartment.setId(ministryDepartment.getId());
+            existingMinistryDepartment.setName(ministryDepartment.getName());
+            existingMinistryDepartment.setType(existingMinistryDepartment.getType());
+            return ministryDepartmentRepository.save(existingMinistryDepartment);
+        }
+        return null;
+    }
+
+    public void deleteMinistryDepartment(Long id) {
+        ministryDepartmentRepository.deleteById(id);
+    }
 }
--- src/main/java/com/teamovercomers/Church/Management/Application/service/PaymentMethodService.java
+++ src/main/java/com/teamovercomers/Church/Management/Application/service/PaymentMethodService.java
@@ -1,4 +1,32 @@
 package com.teamovercomers.Church.Management.Application.service;
 
+import com.teamovercomers.Church.Management.Application.entity.PaymentMethod;
+import com.teamovercomers.Church.Management.Application.repository.PaymentMethodRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+import static com.teamovercomers.Church.Management.Application.repository.PaymentMethodRepository.*;
+
+@Service
 public class PaymentMethodService {
+
+@Autowired
+    private PaymentMethodRepository paymentMethodRepository;
+    public List<PaymentMethod> getAllPaymentMethod() {
+
+        return paymentMethodRepository.findAll();
+    }
+
+    public PaymentMethod getPaymentMethodById(Long id) {
+        return PaymentMethodRepository.findById(id).orElse(null);
+    }
+    public PaymentMethod savePaymentMethod(PaymentMethod paymentMethod) {
+        return paymentMethodRepository.save(paymentMethod);
+    }
+    public void deletePaymentMethod(Integer id) {
+        paymentMethodRepository.deleteById(id);
+    }
+
 }
